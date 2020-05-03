33839c6ebbc4a159e39c5c52622f6ef0f4abe3b3
Ngoc Giang <giang.ngocdao@gmail.com>
2020-05-03 12:48:50 +0700

    free demo style from import

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Image, Grid, Row, Col } from 'react-bootstrap';
+import React, {Component} from 'react'
+import {Grid, Row, Col} from 'react-bootstrap'
 import Modal from './Modal'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 class Images extends Component {
   static defaultProps = {
@@ -10,7 +10,7 @@ class Images extends Component {
     renderOverlay: () => 'Preview Image',
     overlayBackgroundColor: '#222222',
     onClickEach: null,
-    countFrom: 5
+    countFrom: 5,
   }
 
   constructor(props) {
@@ -18,22 +18,23 @@ class Images extends Component {
 
     this.state = {
       modal: false,
-      countFrom: props.countFrom > 0 && props.countFrom < 5 ? props.countFrom : 5,
-      conditionalRender: false
-    };
+      countFrom:
+        props.countFrom > 0 && props.countFrom < 5 ? props.countFrom : 5,
+      conditionalRender: false,
+    }
 
-    this.openModal = this.openModal.bind(this);
-    this.onClose = this.onClose.bind(this);
+    this.openModal = this.openModal.bind(this)
+    this.onClose = this.onClose.bind(this)
 
-    if(props.countFrom <= 0 || props.countFrom > 5) {
+    if (props.countFrom <= 0 || props.countFrom > 5) {
       console.warn('countFrom is limited to 5!')
     }
   }
 
   openModal(index) {
-    const {onClickEach, images} = this.props;
+    const {onClickEach, images} = this.props
 
-    if(onClickEach) {
+    if (onClickEach) {
       return onClickEach({src: images[index], index})
     }
 
@@ -45,101 +46,179 @@ class Images extends Component {
   }
 
   renderOne() {
-    const {images} = this.props;
-    const {countFrom} = this.state;
-    const overlay = images.length > countFrom && countFrom == 1 ? this.renderCountOverlay(true) : this.renderOverlay();
+    const {images} = this.props
+    const {countFrom} = this.state
+    const overlay =
+      images.length > countFrom && countFrom == 1
+        ? this.renderCountOverlay(true)
+        : this.renderOverlay()
 
-    return  <Grid>
-      <Row>
-        <Col xs={12} md={12} className={`border height-one background`} onClick={this.openModal.bind(this, 0)} style={{background: `url(${images[0]})`}}>
-          {overlay}
-        </Col>
-      </Row>
-    </Grid>;
+    return (
+      <Grid>
+        <Row>
+          <Col
+            xs={12}
+            md={12}
+            className={`border height-one background`}
+            onClick={this.openModal.bind(this, 0)}
+            style={{background: `url(${images[0]})`}}>
+            {overlay}
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 
   renderTwo() {
-    const {images} = this.props;
-    const {countFrom} = this.state;
-    const overlay = images.length > countFrom && [2, 3].includes(+countFrom) ? this.renderCountOverlay(true) : this.renderOverlay();
-    const conditionalRender = [3, 4].includes(images.length) || images.length > +countFrom && [3, 4].includes(+countFrom);
+    const {images} = this.props
+    const {countFrom} = this.state
+    const overlay =
+      images.length > countFrom && [2, 3].includes(+countFrom)
+        ? this.renderCountOverlay(true)
+        : this.renderOverlay()
+    const conditionalRender =
+      [3, 4].includes(images.length) ||
+      (images.length > +countFrom && [3, 4].includes(+countFrom))
 
-    return <Grid>
-      <Row>
-        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 0)} style={{background: `url(${conditionalRender ? images[1] : images[0]})`}}>
-          {this.renderOverlay()}
-        </Col>
-        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 1)} style={{background: `url(${conditionalRender ? images[2] : images[1]})`}}>
-          {overlay}
-        </Col>
-      </Row>
-    </Grid>;
+    return (
+      <Grid>
+        <Row>
+          <Col
+            xs={6}
+            md={6}
+            className='border height-two background'
+            onClick={this.openModal.bind(this, conditionalRender ? 1 : 0)}
+            style={{
+              background: `url(${conditionalRender ? images[1] : images[0]})`,
+            }}>
+            {this.renderOverlay()}
+          </Col>
+          <Col
+            xs={6}
+            md={6}
+            className='border height-two background'
+            onClick={this.openModal.bind(this, conditionalRender ? 2 : 1)}
+            style={{
+              background: `url(${conditionalRender ? images[2] : images[1]})`,
+            }}>
+            {overlay}
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 
   renderThree(more) {
-    const {images} = this.props;
-    const {countFrom} = this.state;
-    const overlay = !countFrom || countFrom > 5 || images.length > countFrom && [4, 5].includes(+countFrom) ? this.renderCountOverlay(true) : this.renderOverlay(conditionalRender ? 3 : 4);
-    const conditionalRender = images.length == 4 || images.length > +countFrom && +countFrom == 4;
+    const {images} = this.props
+    const {countFrom} = this.state
+    const overlay =
+      !countFrom ||
+      countFrom > 5 ||
+      (images.length > countFrom && [4, 5].includes(+countFrom))
+        ? this.renderCountOverlay(true)
+        : this.renderOverlay(conditionalRender ? 3 : 4)
+    const conditionalRender =
+      images.length == 4 || (images.length > +countFrom && +countFrom == 4)
 
-    return <Grid>
-      <Row>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 2)} style={{background: `url(${conditionalRender ? images[1] : images[2]})`}}>
-          {this.renderOverlay(conditionalRender ? 1 : 2)}
-        </Col>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 3)} style={{background: `url(${conditionalRender ? images[2] : images[3]})`}}>
-          {this.renderOverlay(conditionalRender ? 2 : 3)}
-        </Col>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 3 : 4)} style={{background: `url(${conditionalRender ? images[3] : images[4]})`}}>
-          {overlay}
-        </Col>
-      </Row>
-    </Grid>;
+    return (
+      <Grid>
+        <Row>
+          <Col
+            xs={6}
+            md={4}
+            className='border height-three background'
+            onClick={this.openModal.bind(this, conditionalRender ? 1 : 2)}
+            style={{
+              background: `url(${conditionalRender ? images[1] : images[2]})`,
+            }}>
+            {this.renderOverlay(conditionalRender ? 1 : 2)}
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            className='border height-three background'
+            onClick={this.openModal.bind(this, conditionalRender ? 2 : 3)}
+            style={{
+              background: `url(${conditionalRender ? images[2] : images[3]})`,
+            }}>
+            {this.renderOverlay(conditionalRender ? 2 : 3)}
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            className='border height-three background'
+            onClick={this.openModal.bind(this, conditionalRender ? 3 : 4)}
+            style={{
+              background: `url(${conditionalRender ? images[3] : images[4]})`,
+            }}>
+            {overlay}
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 
   renderOverlay(id) {
-    const {hideOverlay, renderOverlay, overlayBackgroundColor} = this.props;
+    const {hideOverlay, renderOverlay, overlayBackgroundColor} = this.props
 
-    if(hideOverlay) {
+    if (hideOverlay) {
       return false
     }
 
     return [
-      <div key={`cover-${id}`} className="cover slide" style={{backgroundColor: overlayBackgroundColor}}></div>,
-      <div key={`cover-text-${id}`} className="cover-text slide animate-text"  style={{fontSize: '100%'}}>
+      <div
+        key={`cover-${id}`}
+        className='cover slide'
+        style={{backgroundColor: overlayBackgroundColor}}></div>,
+      <div
+        key={`cover-text-${id}`}
+        className='cover-text slide animate-text'
+        style={{fontSize: '100%'}}>
         {renderOverlay()}
-      </div>
+      </div>,
     ]
   }
 
   renderCountOverlay(more) {
-    const {images} = this.props;
-    const {countFrom} = this.state;
-    const extra = images.length - (countFrom && countFrom > 5 ? 5 : countFrom);
+    const {images} = this.props
+    const {countFrom} = this.state
+    const extra = images.length - (countFrom && countFrom > 5 ? 5 : countFrom)
 
-    return [more && <div key="count" className="cover"></div>, more && <div key="count-sub" className="cover-text" style={{fontSize: '200%'}}><p>+{extra}</p></div>]
+    return [
+      more && <div key='count' className='cover'></div>,
+      more && (
+        <div key='count-sub' className='cover-text' style={{fontSize: '200%'}}>
+          <p>+{extra}</p>
+        </div>
+      ),
+    ]
   }
 
-  render(){
-    const {modal, index, countFrom} = this.state;
-    const {images} = this.props;
-    const imagesToShow = [...images];
+  render() {
+    const {modal, index, countFrom} = this.state
+    const {images} = this.props
+    const imagesToShow = [...images]
 
-    if(countFrom && images.length > countFrom) {
-      imagesToShow.length = countFrom;
+    if (countFrom && images.length > countFrom) {
+      imagesToShow.length = countFrom
     }
 
-    return(
-        <div className="grid-container">
-          {[1, 3, 4].includes(imagesToShow.length)  && this.renderOne()}
-          {imagesToShow.length >= 2 && imagesToShow.length != 4 && this.renderTwo()}
+    return (
+      <div className='pety-images-grid'>
+        <div className='grid-container'>
+          {[1, 3, 4].includes(imagesToShow.length) && this.renderOne()}
+          {imagesToShow.length >= 2 &&
+            imagesToShow.length != 4 &&
+            this.renderTwo()}
           {imagesToShow.length >= 4 && this.renderThree()}
 
-          {modal && <Modal onClose={this.onClose} index={index} images={images}/>}
+          {modal && (
+            <Modal onClose={this.onClose} index={index} images={images} />
+          )}
         </div>
+      </div>
     )
   }
-
 }
 
 Images.propTypes = {
@@ -149,6 +228,6 @@ Images.propTypes = {
   overlayBackgroundColor: PropTypes.string,
   onClickEach: PropTypes.func,
   countFrom: PropTypes.number,
-};
+}
 
-export default Images;
+export default Images
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react';
-import { Image, Modal, Grid, Row, Col } from 'react-bootstrap';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
+import React, {Component} from 'react'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css' // This only needs to be imported once in your app
 
 class ModalComponent extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            images: props.images || [],
-            currentImageIndex: props.index
-        };
-
-        this.onMovePrevRequest = this.onMovePrevRequest.bind(this);
-        this.onMoveNextRequest = this.onMoveNextRequest.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: props.images || [],
+      currentImageIndex: props.index,
     }
 
-    onMovePrevRequest() {
-        const {currentImageIndex, images} = this.state;
+    this.onMovePrevRequest = this.onMovePrevRequest.bind(this)
+    this.onMoveNextRequest = this.onMoveNextRequest.bind(this)
+  }
 
-        this.setState({
-            currentImageIndex: (currentImageIndex + images.length - 1) % images.length,
-        })
-    }
+  onMovePrevRequest() {
+    const {currentImageIndex, images} = this.state
 
-    onMoveNextRequest() {
-        const {currentImageIndex, images} = this.state;
+    this.setState({
+      currentImageIndex:
+        (currentImageIndex + images.length - 1) % images.length,
+    })
+  }
 
-        this.setState({
-            currentImageIndex: (currentImageIndex + 1) % images.length,
-        })
-    }
+  onMoveNextRequest() {
+    const {currentImageIndex, images} = this.state
 
-    render(){
-        const {images, currentImageIndex} = this.state;
-        const {onClose, index} = this.props;
+    this.setState({
+      currentImageIndex: (currentImageIndex + 1) % images.length,
+    })
+  }
 
-        return(
-            <Lightbox
+  render() {
+    const {images, currentImageIndex} = this.state
+    const {onClose, index} = this.props
+
+    return (
+      <Lightbox
         mainSrc={images[currentImageIndex]}
         nextSrc={images[(currentImageIndex + 1) % images.length]}
-        prevSrc={images[(currentImageIndex + images.length - 1) % images.length]}
+        prevSrc={
+          images[(currentImageIndex + images.length - 1) % images.length]
+        }
         onCloseRequest={onClose}
         onMovePrevRequest={this.onMovePrevRequest}
         onMoveNextRequest={this.onMoveNextRequest}
-    />
+      />
     )
-    }
-
+  }
 }
 
-export default ModalComponent;
+export default ModalComponent
