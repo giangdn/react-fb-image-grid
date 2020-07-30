21739d936cf45ddd5ddbba1d4a24fe23e5199f30
Ngoc Giang <giang.ngocdao@gmail.com>
2020-07-30 14:54:41 +0700

    add new building sourcode

--- es/components/Modal.js
+++ es/components/Modal.js
@@ -57,9 +57,9 @@ var ModalComponent = function (_Component) {
 
 
     return React.createElement(Lightbox, {
-      mainSrc: images[currentImageIndex].source,
-      nextSrc: images.length > 1 ? images[(currentImageIndex + 1) % images.length].source : null,
-      prevSrc: images.length > 1 ? images[(currentImageIndex + images.length - 1) % images.length].source : null,
+      mainSrc: images[currentImageIndex].src,
+      nextSrc: images.length > 1 ? images[(currentImageIndex + 1) % images.length].src : null,
+      prevSrc: images.length > 1 ? images[(currentImageIndex + images.length - 1) % images.length].src : null,
       onCloseRequest: onClose,
       onMovePrevRequest: this.onMovePrevRequest,
       onMoveNextRequest: this.onMoveNextRequest,
